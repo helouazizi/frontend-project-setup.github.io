442e119fbdb2623f112a7c5d2fc6d77605eeac09
ouazizi <ouazizi2028@gmail.com>
2024-05-01 00:54:24 +0100

    add hedaer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('it hassan ');
+let shownBtn = document.getElementById('shown-btn');
+let links = document.querySelector('.links');
+let btn = document.querySelector('.btn');
+let hidenBtn = document.createElement('button');
+    hidenBtn.textContent = 'close';
+
+shownBtn.addEventListener('click',function(){
+    links.classList.toggle('show');
+    /*this.onclick = function(){
+        links.classList.add('hide')
+    }*/
+    
+}) 
